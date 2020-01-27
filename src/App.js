c5681fe5d02819a6bee1f6b90cf4464fbe4fc4c6
Maghion <geo.f.medina@gmail.com>
2020-01-27 12:14:34 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,35 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="SallyStudent">
+        <section className='ContactInfo'>
+          <p>Sally Student</p>
+          <p>Email: Sally@Outlook.com </p>
+          <p>555-5555</p>
+          <img src='https://ia.media-imdb.com/images/M/MV5BMTY5NzE3NzU3MF5BMl5BanBnXkFtZTgwMjg0NTQ5MDE@._V1_UX214_CR0,0,214,317_AL_.jpg'></img>
+        </section>
+        <section className='Education'>
+          School : Thinkful
+        </section>
+        <section className='Employment'>
+          <ul>
+            <li>Employer: Anthony's NY Style Pizza</li>
+            <li>Job Title: Deliver Driver</li>
+            <li>Dates: 12/2019 - current </li>
+            <li>Description: Delivery Pizzas</li>
+          </ul>
+          <ul>
+            <li>Employer: Cognizant Technology Solutions</li>
+            <li>Job Title: Process Executive</li>
+            <li>Dates: 7/2017 - 12/2019</li>
+            <li>Description: Moderate content for social media site Facebook and Instagram</li>
+          </ul>
+        </section>
+
       </header>
+      <main>
+
+      </main>
     </div>
   );
 }
